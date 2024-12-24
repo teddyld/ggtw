a57bd649d032c29d2e4da7a4d74a70ba28c6dad6
Vincent <vincent.pham2@outlook.com>
2024-12-24 21:38:27 +1100

    Added more detail to WorkoutPage skeleton in WorkoutLoading component

--- client/src/components/workout/WorkoutLoading.tsx
+++ client/src/components/workout/WorkoutLoading.tsx
@@ -1,17 +1,60 @@
-import { Skeleton } from "@mantine/core";
+import { Stack, Group, Skeleton } from "@mantine/core";
 
 import Layout from "../layout/Layout";
 import Container from "../layout/Container";
 
+function WorkoutSkeleton() {
+  return (
+    <Container w={472}>
+      <Skeleton height={60} mb="md" />
+      <Group wrap="nowrap" justify="space-between" mb="md">
+        <Skeleton height={28} width={200} />
+        <Skeleton height={28} width={30} />
+      </Group>
+      <Skeleton height={22} width="100%" mb="md" />
+      <Group mb="sm" wrap="nowrap">
+        <Skeleton height={40} w={100} />
+        <Skeleton height={40} />
+        <Skeleton height={40} />
+      </Group>
+      <Group mb="sm" wrap="nowrap">
+        <Skeleton height={40} w={100} />
+        <Skeleton height={40} />
+        <Skeleton height={40} />
+      </Group>
+      <Group mb="md" wrap="nowrap">
+        <Skeleton height={40} w={100} />
+        <Skeleton height={40} />
+        <Skeleton height={40} />
+      </Group>
+      <Group wrap="nowrap" justify="space-between" mb="md">
+        <Skeleton height={28} width={200} />
+        <Skeleton height={28} width={30} />
+      </Group>
+      <Skeleton height={22} width="100%" mb="md" />
+      <Group mb="sm" wrap="nowrap">
+        <Skeleton height={40} w={100} />
+        <Skeleton height={40} />
+        <Skeleton height={40} />
+      </Group>
+      <Group mb="sm" wrap="nowrap">
+        <Skeleton height={40} w={100} />
+        <Skeleton height={40} />
+        <Skeleton height={40} />
+      </Group>
+      <Group wrap="nowrap">
+        <Skeleton height={40} w={100} />
+        <Skeleton height={40} />
+        <Skeleton height={40} />
+      </Group>
+    </Container>
+  );
+}
+
 export default function WorkoutLoading() {
   return (
     <Layout>
-      <Container p={0} w={472} >
-        <Skeleton height={500} />
-      </Container>
-      <Container p={0} w={472} >
-        <Skeleton height={500} />
-      </Container>
+      <WorkoutSkeleton />
     </Layout>
   );
 }
