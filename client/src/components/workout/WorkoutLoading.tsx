3199eb5c0dddd58f95c718a5a9ffd5b5f6022ee3
Vincent <vincent.pham2@outlook.com>
2024-12-26 22:47:08 +1100

    Update WorkoutLoading skeleton

--- client/src/components/workout/WorkoutLoading.tsx
+++ client/src/components/workout/WorkoutLoading.tsx
@@ -6,12 +6,9 @@ import Container from "../layout/Container";
 function WorkoutSkeleton() {
   return (
     <Container w={472}>
-      <Skeleton height={60} mb="md" />
-      <Group wrap="nowrap" justify="space-between" mb="md">
-        <Skeleton height={28} width={200} />
-        <Skeleton height={28} width={30} />
-      </Group>
-      <Skeleton height={22} width="100%" mb="md" />
+      <Skeleton height={40} mb="lg" />
+      <Skeleton height={28} width={200} mb="sm" />
+      <Skeleton height={22} width="25%" mb="xl" />
       <Group mb="sm" wrap="nowrap">
         <Skeleton height={40} w={100} />
         <Skeleton height={40} />
@@ -22,16 +19,13 @@ function WorkoutSkeleton() {
         <Skeleton height={40} />
         <Skeleton height={40} />
       </Group>
-      <Group mb="md" wrap="nowrap">
+      <Group mb="xl" wrap="nowrap">
         <Skeleton height={40} w={100} />
         <Skeleton height={40} />
         <Skeleton height={40} />
       </Group>
-      <Group wrap="nowrap" justify="space-between" mb="md">
-        <Skeleton height={28} width={200} />
-        <Skeleton height={28} width={30} />
-      </Group>
-      <Skeleton height={22} width="100%" mb="md" />
+      <Skeleton height={28} width={200} mb="sm" />
+      <Skeleton height={22} width="100%" mb="xl" />
       <Group mb="sm" wrap="nowrap">
         <Skeleton height={40} w={100} />
         <Skeleton height={40} />
