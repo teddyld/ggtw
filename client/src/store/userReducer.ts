b69bce58ef18865c2903263d06a53b2b80579510
Vincent <vincent.pham2@outlook.com>
2025-01-07 23:56:27 +1100

    Created thunks for updating and deleting user workouts in userReducer

--- client/src/store/userReducer.ts
+++ client/src/store/userReducer.ts
@@ -1,28 +1,103 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { programState } from "../components/workout/workoutData";
+import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
+import { workoutType } from "../components/workout/workoutData";
+import axios from "axios";
 
 type userState = {
   id: string;
-  program: programState;
+  userWorkouts: workoutType[];
+  status: "idle" | "pending" | "succeeded" | "failed";
+  error: string | null;
 };
 
 const initialState: userState = {
   id: "",
-  program: [],
+  userWorkouts: [], // Facilitate client fetching of boards
+  status: "idle",
+  error: null,
 };
+
+export const setWorkout = createAsyncThunk(
+  "workout/update",
+  async ({ userId, workout }: { userId: string; workout: workoutType }) => {
+    await axios.put("user/workout/update", {
+      userId,
+      workout,
+    });
+    return workout;
+  },
+);
+
+export const deleteWorkout = createAsyncThunk(
+  "workout/delete",
+  async ({ userId, workoutId }: { userId: string; workoutId: string }) => {
+    await axios.put("user/workout/delete", {
+      userId,
+      workoutId,
+    });
+    return workoutId;
+  },
+);
 
 const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setStoreId: (state, action: PayloadAction<string>) => {
+    setUserId: (state, action: PayloadAction<string>) => {
       state.id = action.payload;
     },
-    setStoreProgram: (state, action: PayloadAction<programState>) => {
-      state.program = action.payload;
+    setUserWorkouts: (state, action: PayloadAction<workoutType[]>) => {
+      state.userWorkouts = action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(setWorkout.pending, (state, _) => {
+        state.status = "pending";
+      })
+      .addCase(
+        setWorkout.fulfilled,
+        (state, action: PayloadAction<workoutType>) => {
+          state.status = "succeeded";
+          const newWorkouts = Array.from(state.userWorkouts);
+          const workoutIndex = newWorkouts.findIndex(
+            (workout) => workout.id === action.payload.id,
+          );
+
+          // Add to userWorkouts if workout does not exist
+          if (workoutIndex === -1) {
+            newWorkouts.push(action.payload);
+          } else {
+            newWorkouts[workoutIndex] = action.payload;
+          }
+
+          state.userWorkouts = newWorkouts;
+        },
+      )
+      .addCase(setWorkout.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message ?? "Unknown error";
+      })
+      .addCase(deleteWorkout.pending, (state, _) => {
+        state.status = "pending";
+      })
+      .addCase(
+        deleteWorkout.fulfilled,
+        (state, action: PayloadAction<string>) => {
+          state.status = "succeeded";
+          const newWorkouts = Array.from(state.userWorkouts);
+          const workoutIndex = newWorkouts.findIndex(
+            (workout) => workout.id === action.payload,
+          );
+          newWorkouts.splice(workoutIndex, 1);
+          state.userWorkouts = newWorkouts;
+        },
+      )
+      .addCase(deleteWorkout.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message ?? "Unknown error";
+      });
   },
 });
 
-export const { setStoreId, setStoreProgram } = userSlice.actions;
+export const { setUserId, setUserWorkouts } = userSlice.actions;
 export default userSlice.reducer;
