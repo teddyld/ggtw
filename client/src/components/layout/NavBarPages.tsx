36bdad9e22cbb9653897e019d34e2bcaf3ec835b
Vincent <vincent.pham2@outlook.com>
2025-01-22 00:23:49 +1100

    Update NavBar to close in mobile layout

--- client/src/components/layout/NavBarPages.tsx
+++ client/src/components/layout/NavBarPages.tsx
@@ -3,13 +3,19 @@ import { Stack, Group, Text, Tooltip } from "@mantine/core";
 import { pageInfo } from "../../pages/pageInfo";
 import NavBarButton from "./NavBarButton";
 
-export default function NavBarPages({ opened }: { opened: boolean }) {
+export default function NavBarPages({
+  opened,
+  toggle,
+}: {
+  opened: boolean;
+  toggle: () => void;
+}) {
   return (
     <Stack align="stretch" justify="flex-start" gap="xs">
       {pageInfo.map((page, key) => {
         if (opened) {
           return (
-            <NavBarButton page={page} key={`page-btn-${key}`}>
+            <NavBarButton page={page} key={`page-btn-${key}`} toggle={toggle}>
               <Group>
                 <page.icon className="text-lg" />
                 <Text size="md">{page.name}</Text>
@@ -19,7 +25,7 @@ export default function NavBarPages({ opened }: { opened: boolean }) {
         } else {
           return (
             <Tooltip label={page.name} position="right" key={`page-btn-${key}`}>
-              <NavBarButton page={page}>
+              <NavBarButton page={page} toggle={toggle}>
                 <page.icon className="text-lg" />
               </NavBarButton>
             </Tooltip>
