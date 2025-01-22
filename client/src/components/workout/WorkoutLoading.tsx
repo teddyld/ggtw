3579acaa60a37961d23568f43a0981bd5afc0367
Vincent <vincent.pham2@outlook.com>
2025-01-22 15:16:26 +1100

    Update WorkoutLoading

--- client/src/components/workout/WorkoutLoading.tsx
+++ client/src/components/workout/WorkoutLoading.tsx
@@ -36,6 +36,23 @@ function WorkoutSkeleton() {
         <Skeleton height={40} />
         <Skeleton height={40} />
       </Group>
+      <Group mb="xl" wrap="nowrap">
+        <Skeleton height={40} w={100} />
+        <Skeleton height={40} />
+        <Skeleton height={40} />
+      </Group>
+      <Skeleton height={28} width={200} mb="sm" />
+      <Skeleton height={22} width="25%" mb="xl" />
+      <Group mb="sm" wrap="nowrap">
+        <Skeleton height={40} w={100} />
+        <Skeleton height={40} />
+        <Skeleton height={40} />
+      </Group>
+      <Group mb="sm" wrap="nowrap">
+        <Skeleton height={40} w={100} />
+        <Skeleton height={40} />
+        <Skeleton height={40} />
+      </Group>
       <Group wrap="nowrap">
         <Skeleton height={40} w={100} />
         <Skeleton height={40} />
@@ -48,8 +65,6 @@ function WorkoutSkeleton() {
 export default function WorkoutLoading() {
   return (
     <Layout>
-      <WorkoutSkeleton />
-      <WorkoutSkeleton />
       <WorkoutSkeleton />
     </Layout>
   );
