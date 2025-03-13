c3833cb4d72613266230545fb6b4f0c239ccbca0
Vincent <vincent.pham2@outlook.com>
2025-03-13 15:05:44 +1100

    Update exerciseRecordType from optional to undefined

--- client/src/components/statistics/statisticsData.tsx
+++ client/src/components/statistics/statisticsData.tsx
@@ -12,10 +12,12 @@ export type logType = {
 };
 
 type loggedExercises = Record<string, logValueType[]>;
+export type pbType = Record<string, logValueType & { date: string }>;
 
 export type statisticsType = {
   activity: Record<string, loggedExercises>;
   exercises: Record<string, string[]>;
+  personalBests: pbType;
   startDate: Date;
 };
 
@@ -27,13 +29,13 @@ export type activityType = {
 export type exerciseRecordType = {
   date: string;
   "MAX Weight": number;
-  "MAX Reps"?: number;
-  "MAX Time"?: number;
+  "MAX Reps": number | undefined;
+  "MAX Time": number | undefined;
 };
 
 export type summaryType = {
   exerciseName: string;
-  previousBest: exerciseRecordType;
+  personalBest: exerciseRecordType;
   currentRecord: exerciseRecordType;
   status: "UP" | "DOWN" | "NO CHANGE";
 };
@@ -83,33 +85,9 @@ export const createMaxRecord = (
       year: "numeric",
     }),
     "MAX Weight": maxSet.weight,
+    "MAX Reps": maxSet.reps,
+    "MAX Time": maxSet.time,
   };
-
-  if (maxSet.reps !== -1) {
-    exerciseRecord["MAX Reps"] = maxSet.reps;
-  }
-
-  if (maxSet.time !== -1) {
-    exerciseRecord["MAX Time"] = maxSet.time;
-  }
 
   return exerciseRecord;
 };
-
-export const mockExerciseData: exerciseRecordType[] = [
-  { date: "15 Jan, 2023", "MAX Weight": 90, "MAX Reps": 12 },
-  { date: "20 Feb, 2023", "MAX Weight": 95, "MAX Reps": 10 },
-  { date: "10 May, 2023", "MAX Weight": 110 },
-  { date: "04 Jul, 2023", "MAX Weight": 105, "MAX Reps": 8, "MAX Time": 50 },
-  { date: "31 Oct, 2023", "MAX Weight": 115 },
-  { date: "01 Jan, 2024", "MAX Weight": 120, "MAX Reps": 6 },
-  { date: "15 Mar, 2024", "MAX Weight": 125, "MAX Time": 40 },
-  { date: "21 Jun, 2024", "MAX Weight": 130, "MAX Reps": 5 },
-  { date: "10 Sep, 2024", "MAX Weight": 128 },
-  { date: "25 Dec, 2024", "MAX Weight": 135, "MAX Reps": 4, "MAX Time": 30 },
-  { date: "14 Feb, 2025", "MAX Weight": 140, "MAX Reps": 3 },
-  { date: "01 May, 2025", "MAX Weight": 145, "MAX Time": 25 },
-  { date: "20 Aug, 2025", "MAX Weight": 150, "MAX Reps": 2 },
-  { date: "11 Nov, 2025", "MAX Weight": 155 },
-  { date: "01 Jan, 2026", "MAX Weight": 160, "MAX Reps": 1, "MAX Time": 20 },
-];
